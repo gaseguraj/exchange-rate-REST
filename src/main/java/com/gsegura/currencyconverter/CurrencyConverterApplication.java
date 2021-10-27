99da15144a344da2252b29fc6fe5c37cd3a4b981
German Segura <german.andres.segura@gmail.com>
2021-10-27 18:00:44 -0400

    Adding missing files

---
+++ src/main/java/com/gsegura/currencyconverter/CurrencyConverterApplication.java
@@ -0,0 +1,22 @@
+package com.gsegura.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	   return builder.build();
+	}
+}
